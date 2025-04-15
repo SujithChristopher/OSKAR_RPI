9bc41d0f3c7b7852733ccae1e3e808a17899c894
SujithChristopher <chrisbon95@gmail.com>
2025-04-15 14:41:27 +0530

    ...

--- Results/Scripts/parse_files.gd
+++ Results/Scripts/parse_files.gd
@@ -1,21 +1,30 @@
 extends Node
 
-var path = GlobalSignals.data_path + "//sujith"
+var path = GlobalSignals.data_path + "//" + GlobalSignals.current_patient_id
+
+# var current_patient_id = GlobalSignals.current_patient_id
+var parsed_data = []
 
 func _ready() -> void:
 	print("Parsing files in: " + path)
+	parse_workspace_files()
+
+func parse_workspace_files() -> void:
 	var dir = DirAccess.open(path)
 	if dir:
 		dir.list_dir_begin()
 		var file_name = dir.get_next()
 		while file_name != "":
 			if !dir.current_is_dir() and file_name.ends_with(".json"):
-				parse_workspace_file(path + "/" + file_name)
+				var file_data = parse_workspace_file(path + "/" + file_name)
+				if file_data:
+					parsed_data.append(file_data)
 			file_name = dir.get_next()
+		parsed_data.sort_custom(func(a, b): return a.date < b.date)
 	else:
 		print("An error occurred when trying to access the path.")
 
-func parse_workspace_file(file_path: String) -> void:
+func parse_workspace_file(file_path: String) -> Dictionary:
 	var file = FileAccess.open(file_path, FileAccess.READ)
 	if file:
 		var content = file.get_as_text()
@@ -31,6 +40,9 @@ func parse_workspace_file(file_path: String) -> void:
 			if data.has("active_workspace") and data.has("axdir") and data.has("azdir") and \
 			   data.has("inflated_workspace") and data.has("txdir") and data.has("tzdir"):
 				
+				# Extract date from filename (assuming format: workspace-YYYY-MM-DD.json)
+				var date_str = file_path.get_file().trim_prefix("workspace-").trim_suffix(".json")
+				
 				print("  Active workspace:")
 				print("    axdir: " + str(data["axdir"]))
 				print("    azdir: " + str(data["azdir"]))
@@ -39,17 +51,28 @@ func parse_workspace_file(file_path: String) -> void:
 				print("    txdir: " + str(data["txdir"]))
 				print("    tzdir: " + str(data["tzdir"]))
 				
-				# Optional: Parse the polygon string to Vector2 array if needed
+				# Optional: Parse the polygon string to Vector2 array
 				var active_poly = parse_polygon_string(data["active_workspace"])
 				var inflated_poly = parse_polygon_string(data["inflated_workspace"])
 				
-				# Do something with the parsed polygons if needed
+				return {
+					"filename": file_path.get_file(),
+					"date": date_str,
+					"axdir": data["axdir"],
+					"azdir": data["azdir"],
+					"txdir": data["txdir"],
+					"tzdir": data["tzdir"],
+					"active_poly": active_poly,
+					"inflated_poly": inflated_poly
+				}
 			else:
 				print("  Missing required workspace data")
 		else:
 			print("  JSON Parse Error: " + str(error) + " at line " + str(json.get_error_line()))
 	else:
 		print("  Failed to open file: " + file_path)
+	
+	return {}
 
 func parse_polygon_string(polygon_string: String) -> Array[Vector2]:
 	var result: Array[Vector2] = []
@@ -69,3 +92,6 @@ func parse_polygon_string(polygon_string: String) -> Array[Vector2]:
 			result.append(Vector2(x, y))
 	
 	return result
+
+func get_parsed_data() -> Array:
+	return parsed_data
--- Results/Scripts/user_progress.gd
+++ Results/Scripts/user_progress.gd
@@ -2,83 +2,90 @@ extends Control
 
 @onready var chart: Chart = $Chart
 
-# This Chart will plot 3 different functions
-var f1: Function
-var f2: Function
-var f3: Function
-
 func _ready():
-    # Let's create our @x values
+	# Get data from the parser
+	var parser = preload("res://Results/Scripts/parse_files.gd").new()
+	parser._ready()
+	var workspace_data = parser.get_parsed_data()
+	
+	if workspace_data.size() == 0:
+		print("No data to plot")
+		return
+	
+	# Prepare data for plotting
+	var x_values = range(workspace_data.size())
+	var axdir_values = []
+	var azdir_values = []
+	var txdir_values = []
+	var tzdir_values = []
+	var date_labels = []
+	
+	for data in workspace_data:
+		axdir_values.append(data.axdir)
+		azdir_values.append(data.azdir)
+		txdir_values.append(data.txdir)
+		tzdir_values.append(data.tzdir)
+		date_labels.append(data.date)
+	
+	# Create functions for each directional value
+	var f_axdir = Function.new(
+		x_values, axdir_values, "Active Workspace axdir", 
+		{
+			color = Color("#36a2eb"),
+			marker = Function.Marker.CIRCLE,
+			type = Function.Type.LINE
+		}
+	)
+	
+	var f_azdir = Function.new(
+		x_values, azdir_values, "Active Workspace azdir", 
+		{
+			color = Color("#ff6384"),
+			marker = Function.Marker.CROSS,
+			type = Function.Type.LINE
+		}
+	)
+	
+	var f_txdir = Function.new(
+		x_values, txdir_values, "Inflated Workspace txdir", 
+		{
+			color = Color("#4bc0c0"),
+			marker = Function.Marker.SQUARE,
+			type = Function.Type.LINE
+		}
+	)
+	
+	var f_tzdir = Function.new(
+		x_values, tzdir_values, "Inflated Workspace tzdir", 
+		{
+			color = Color("#ffcd56"),
+			marker = Function.Marker.TRIANGLE,
+			type = Function.Type.LINE
+		}
+	)
+	
+	# Set up chart properties
+	var cp = ChartProperties.new()
+	cp.colors.frame = Color("#161a1d")
+	cp.colors.background = Color.TRANSPARENT
+	cp.colors.grid = Color("#283442")
+	cp.colors.ticks = Color("#283442")
+	cp.colors.text = Color.WHITE_SMOKE
+	cp.draw_bounding_box = false
+	cp.show_legend = true
+	cp.title = "Workspace Directional Values"
+	cp.x_label = "Session Date"
+	cp.y_label = "Direction Values (degrees)"
+	cp.x_scale = 1
+	cp.y_scale = 5
+	cp.interactive = true
+	# cp.x_labels = date_labels
+	
+	# Plot the data
+	chart.plot([f_axdir, f_azdir, f_txdir, f_tzdir], cp)
+	
+	print("Chart created with " + str(workspace_data.size()) + " data points")
 
-    var parse_files = preload("res://Results/Scripts/parse_files.gd").new()
-    # parse_files._ready()
 
-    var x: Array = ArrayOperations.multiply_float(range(-10, 11, 1), 0.5)
-    
-    # And our y values. It can be an n-size array of arrays.
-    # NOTE: `x.size() == y.size()` or `x.size() == y[n].size()`
-    var y: Array = ArrayOperations.multiply_int(ArrayOperations.cos(x), 20)
-    var y2: Array = ArrayOperations.add_float(ArrayOperations.multiply_int(ArrayOperations.sin(x), 20), 20)
-    var y3: Array = ArrayOperations.add_float(ArrayOperations.multiply_int(ArrayOperations.cos(x), -5), -3)
-    
-    # Let's customize the chart properties, which specify how the chart
-    # should look, plus some additional elements like labels, the scale, etc...
-    var cp: ChartProperties = ChartProperties.new()
-    cp.colors.frame = Color("#161a1d")
-    cp.colors.background = Color.TRANSPARENT
-    cp.colors.grid = Color("#283442")
-    cp.colors.ticks = Color("#283442")
-    cp.colors.text = Color.WHITE_SMOKE
-    cp.draw_bounding_box = false
-    cp.show_legend = true
-    cp.title = "NOARK Assessment"
-    cp.x_label = "Session"
-    cp.y_label = "cm squared"
-    cp.x_scale = 5
-    cp.y_scale = 10
-    cp.interactive = true # false by default, it allows the chart to create a tooltip to show point values
-    # and interecept clicks on the plot
-    
-    # Let's add values to our functions
-    f1 = Function.new(
-        x, y, "Workspace", # This will create a function with x and y values taken by the Arrays 
-                        # we have created previously. This function will also be named "Pressure"
-                        # as it contains 'pressure' values.
-                        # If set, the name of a function will be used both in the Legend
-                        # (if enabled thourgh ChartProperties) and on the Tooltip (if enabled).
-        # Let's also provide a dictionary of configuration parameters for this specific function.
-        { 
-            color = Color("#36a2eb"), 		# The color associated to this function
-            marker = Function.Marker.NONE, 	# The marker that will be displayed for each drawn point (x,y)
-                                            # since it is `NONE`, no marker will be shown.
-            type = Function.Type.AREA, 		# This defines what kind of plotting will be used, 
-                                            # in this case it will be an Area Chart.
-            interpolation = Function.Interpolation.STAIR	# Interpolation mode, only used for 
-                                                            # Line Charts and Area Charts.
-        }
-    )
-    f2 = Function.new(x, y2, "Speed", { color = Color("#ff6384"), marker = Function.Marker.CROSS })
-    f3 = Function.new(x, y3, "Smoothness", { color = Color.GREEN, marker = Function.Marker.TRIANGLE })
-    
-    # Now let's plot our data
-    chart.plot([f1, f2, f3], cp)
-    
-    # Uncommenting this line will show how real time data plotting works
-    set_process(false)
-
-
-var new_val: float = 4.5
-
-func _process(delta: float):
-    # This function updates the values of a function and then updates the plot
-    new_val += 5
-    
-    # we can use the `Function.add_point(x, y)` method to update a function
-    f1.add_point(new_val, cos(new_val) * 20)
-    f2.add_point(new_val, (sin(new_val) * 20) + 20)
-    f3.add_point(new_val, (cos(new_val) * -5) - 3)
-    chart.queue_redraw() # This will force the Chart to be updated
-
-
-# func _on_CheckButton_pressed():
-#     set_process(not is_processing())
+func _on_logout_pressed() -> void:
+	get_tree().change_scene_to_file("res://Main_screen/main.tscn")
