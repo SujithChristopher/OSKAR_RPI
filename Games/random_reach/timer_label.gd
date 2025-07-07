daf5986f0791fc44c3b59702198c43bc1c99b9b6
Yuvanesh24 <yuvansankar2424@gamil.com>
2025-07-07 09:06:05 +0300

    Proper camera closing

--- Games/random_reach/timer_label.gd
+++ Games/random_reach/timer_label.gd
@@ -3,7 +3,7 @@ extends CanvasLayer
 @onready var timer_label = $TimerLabel
 
 func _process(delta):
-	var time = GlobalTimer.get_time()
-	var minutes = int(time) / 60
-	var seconds = int(time) % 60
-	timer_label.text = "%02d:%02d" % [minutes, seconds]
+    var time = GlobalTimer.get_time()
+    var minutes = int(time) / 60
+    var seconds = int(time) % 60
+    timer_label.text = "%02d:%02d" % [minutes, seconds]
--- Main_screen/Scripts/main_window.gd
+++ Main_screen/Scripts/main_window.gd
@@ -12,47 +12,48 @@ var patient_db: PatientDetails = load("res://Main_screen/patient_register.tres")
 
 
 func _on_exit_button_pressed():
-	GlobalSignals.SignalBus.emit()
-	get_tree().quit()
-	
+    GlobalScript.handle_quit_request()
+    GlobalSignals.SignalBus.emit()
+    get_tree().quit()
+    
 func _on_pressed():
-	hosp_id = $"../TextureRect/HospID".text
-	if patient_name == "" and hosp_id == "":
-		popup.show()
-	else:
-		if patient_db.get_patient(hosp_id):
-			patient_db.current_patient_id = hosp_id
-			ResourceSaver.save(patient_db, "res://Main_screen/patient_register.tres")			
-			get_tree().change_scene_to_file("res://Main_screen/select_game.tscn") # Replace with function body.\
-		else:
-			patient_notfound.show()
+    hosp_id = $"../TextureRect/HospID".text
+    if patient_name == "" and hosp_id == "":
+        popup.show()
+    else:
+        if patient_db.get_patient(hosp_id):
+            patient_db.current_patient_id = hosp_id
+            ResourceSaver.save(patient_db, "res://Main_screen/patient_register.tres")			
+            get_tree().change_scene_to_file("res://Main_screen/select_game.tscn") # Replace with function body.\
+        else:
+            patient_notfound.show()
 
 
 func _on_window_close_requested() -> void:
-	popup.hide()
+    popup.hide()
 
 func _on_new_patient_pressed() -> void:
-	get_tree().change_scene_to_file("res://Main_screen/registry.tscn") 
-	
+    get_tree().change_scene_to_file("res://Main_screen/registry.tscn") 
+    
 
 func _on_assess_button_pressed() -> void:
-	ResourceSaver.save(patient_db, "res://Main_screen/patient_register.tres")
+    ResourceSaver.save(patient_db, "res://Main_screen/patient_register.tres")
 
 
 func _on_patient_nf_ok_pressed() -> void:
-	patient_notfound.hide()
+    patient_notfound.hide()
 
 
 func _on_hosp_id_text_submitted(new_text: String) -> void:
-	hosp_id = $"../TextureRect/HospID".text
-	if patient_name == "" and hosp_id == "":
-		popup.show()
-	else:
-		if patient_db.get_patient(hosp_id):
-			patient_db.current_patient_id = hosp_id
-			GlobalScript.change_patient()
-			GlobalSignals.current_patient_id = hosp_id
-			ResourceSaver.save(patient_db, "res://Main_screen/patient_register.tres")
-			get_tree().change_scene_to_file("res://Main_screen/select_game.tscn") 
-		else:
-			patient_notfound.show()
+    hosp_id = $"../TextureRect/HospID".text
+    if patient_name == "" and hosp_id == "":
+        popup.show()
+    else:
+        if patient_db.get_patient(hosp_id):
+            patient_db.current_patient_id = hosp_id
+            GlobalScript.change_patient()
+            GlobalSignals.current_patient_id = hosp_id
+            ResourceSaver.save(patient_db, "res://Main_screen/patient_register.tres")
+            get_tree().change_scene_to_file("res://Main_screen/select_game.tscn") 
+        else:
+            patient_notfound.show()
